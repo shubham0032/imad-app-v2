0a46c774149c0b74272c8bb712049ebefe2162a0
shubham0032 <shubham45gaur@gmail.com>
2017-02-21 01:48:58 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/new.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'new.html'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
